26342b32a05f28856b01c818f885e0ed9ddaeda3
Raghuveer Singh <raghu1213@hotmail.com>
2017-11-05 02:46:19 +0800

    mongo on cloud

--- env.js
+++ env.js
@@ -1,1 +1,3 @@
-module.exports = { mongoDB: "mongodb://127.0.0.1:27017/hamsterdb" }
+//module.exports = { mongoDB: "mongodb://127.0.0.1:27017/hamsterdb" }
+module.exports = { mongoDB: "mongodb://hamsteradmin:CS%40Hack2017@ds149335.mlab.com:49335/heroku_4vgckt94" }
+//const url = "mongodb://mongo-hamster:CS%40Hack2017@ds115573.mlab.com:15573/mongo-hamster";
