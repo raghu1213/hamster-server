fbcdf63d673a4859f4d3a04d722f13c302aa8e19
Raghuveer Singh <raghu1213@hotmail.com>
2017-11-06 18:12:59 +0800

    fix portfolio fetching for a given risk score

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,10 +1,12 @@
 import PortfolioPredict from "../src/predictor/portfolio";
 import * as Helpers from '../src/utils/helper'
+import Logger from '../src/utils/logging'
 
 var express = require('express');
 var router = express.Router();
 var PortfolioWeightSchema = require('../src/db/mongo/pwSchema');
 
+var logger = new Logger()
 
 router.get('/suggest', async function (req, res, next) {
     let result = await PortfolioWeightSchema.find().exec()
@@ -31,9 +33,9 @@ router.get('/suggest/summary', async function (req, res, next) {
 router.get('/suggest/:riskscore', async function (req, res, next) {
 
     let porfolioPredict = new PortfolioPredict();
-    let suggestedPortfolio = porfolioPredict.getRiskAdjustedPortfolio(req.params.riskscore)
+    let suggestedPortfolio = await porfolioPredict.getRiskAdjustedPortfolio(req.params.riskscore)
 
-    suggestedPortfolio = Helpers.formatPortfolio(suggestedPortfolio)
+    suggestedPortfolio =  Helpers.formatPortfolio(suggestedPortfolio)
 
     logger.log("Update Respose-->" + JSON.stringify(suggestedPortfolio))
     res.json(suggestedPortfolio);
