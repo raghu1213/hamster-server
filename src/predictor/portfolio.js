eb0ad23d6c0bf9d1982a43448a706cfad9913f56
anilmodest <modest.anil@gmail.com>
2017-11-08 04:34:09 +0800

    validation applied

--- src/predictor/portfolio.js
+++ src/predictor/portfolio.js
@@ -1,6 +1,9 @@
 var PwSchema = require('../db/mongo/pwSchema')
 import Logger from '../utils/logging'
 import * as Helpers from '../utils/helper'
+var ClientTranasctionSchema = require('../db/mongo/clientTransactionSchema');
+var StockTimeSeries = require('../db/mongo/stockTimeSeries')
+
 
 let logger = new Logger();
 
@@ -18,6 +21,55 @@ export default class Portfolio {
         else {
             return 43
         }
+    }
+
+    async getPortfolioPositionAsOfToday(cif, portfolioId, prevDate, date) {
+        var dict = {};
+        let transactions = await ClientTranasctionSchema.find().byCustomerAndPortfolio(cif, portfolioId).sort({"date": -1}).exec();
+        let stockDetails = await StockTimeSeries.find().byDateRange(prevDate, date).sort({"date": -1}).exec()
+        for(let trans in transactions) {
+            let data = transactions[trans]
+            let units = data.numberOfUnits
+            if(data.BuySell === 'S') {
+                units = -units
+            }
+            if(dict[data.ticker] === undefined || dict[data.ticker] === null) {
+                dict[data.ticker] = units
+            }
+            else {
+                dict[data.ticker] += units
+            }
+        }
+
+        let currentPosition = 0
+        for(let key in dict){
+            let eodPrice = this._getEODPriceForTicker(key, stockDetails)
+            currentPosition += parseInt(dict[key]) * parseFloat(eodPrice)
+        }
+        return currentPosition
+    }
+
+    async getRealizedGain(cif, portfolioId) {
+        var dict = {};
+        let transactions = await ClientTranasctionSchema.find().byCustomerAndPortfolio(cif, portfolioId).sort({"date": -1}).exec();
+        for(let trans in transactions) {
+            let data = transactions[trans]
+            if(data.BuySell === 'B') {
+                continue
+            }
+            if(dict[data.ticker] === undefined || dict[data.ticker] === null) {
+                dict[data.ticker] = parseInt(data.numberOfUnits) * parseFloat(data.unitPrice)
+            }
+            else {
+                dict[data.ticker] += parseInt(data.numberOfUnits) * parseFloat(data.unitPrice)
+            }
+        }
+
+        let realizedGain = 0
+        for(let key in dict){
+            realizedGain += dict[key]
+        }
+        return realizedGain
     }
 
     async getRiskAdjustedPortfolio(riskScore) {
@@ -42,5 +94,14 @@ export default class Portfolio {
             return {};
         }
         return portfolioWeight;
+    }
+
+    _getEODPriceForTicker(ticker, stockEODData){
+        for(let stockIndex in stockEODData) {
+            if(stockEODData[stockIndex].ticker.toLowerCase() === ticker.toLowerCase()){
+                return parseFloat(stockEODData[stockIndex].close)
+            }
+        }
+        return -1
     }
 }
